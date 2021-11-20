6bacd428a7c5e8880a01b40a35391f1331c69433
Jan Georg Henriksen <62566202+jghen@users.noreply.github.com>
2021-11-20 16:04:49 +0100

    Added 3 digit FDV - cleaned up moving

--- nyFDV.ps1
+++ nyFDV.ps1
@@ -1,7 +1,7 @@
 '<------------------------- ENDRE NAVN - NY FDV --------------------------->
 '
 #inputs
-$oldPath = Read-Host ‘Lim inn lokasjonen på NY FDV, F. eks [U:\500000\FDV-dokumentasjon\Skoler\00 Lade skole] ‘
+$oldPath = Read-Host 'Lim inn lokasjonen på NY FDV, F. eks [U:\500000\FDV-dokumentasjon\Skoler\00 Lade skole]'
 Set-Location $oldPath
 $year_built = Read-Host 'Skriv inn byggeår [yyyy]: '
 
@@ -12,15 +12,26 @@ $year_built = Read-Host 'Skriv inn byggeår [yyyy]: '
 $newMainFolder = "00 FDV - med nye filnavn"
 $path = $oldPath + '\' + $newMainFolder
 
-write-output 'Creating new main folder:' $newMainFolder
-write-output 'Copying everything to new path:' $path
+#Delete if new main folder already exists:
+foreach ($child in $children) {
+    if ($child.Name -eq $newMainFolder) {
+        $child | Remove-Item -Force -Recurse
+    }
+}
 
 $children = Get-Childitem
-New-Item -Path $oldPath -Name $newMainFolder -ItemType "directory" 
+
+write-Host ('Creating new main folder:' + $newMainFolder)
+write-Host ('Copying everything to new path:' + $path)
+
+#create new main folder
+New-Item -Path $oldPath -Name $newMainFolder -ItemType "directory"
 
 foreach ($item in $children) {
-    Write-Output 'Copying:  ' $item ' to new folder: ' $newMainFolder 
-    $item | Copy-Item -Destination $path -Recurse
+    if ($item.Name -ne $newMainFolder) {
+        Write-Host ('Copying:  ' + $item + ' to new folder: ' + $newMainFolder)
+        $item | Copy-Item -Destination $path -Recurse
+    } 
 }
 
 Set-Location $path
@@ -31,7 +42,7 @@ foreach ($folder in (Get-ChildItem -Recurse -Directory)) {
         $folder | Rename-Item -NewName {'17 - ' + $folder.Name}
     }
 }
-Write-Output 'Copy complete'
+Write-Host ('Copy complete')
 
 '
 <----------------------------- ZIP-FILER PAKKES UT -------------------------->
@@ -46,29 +57,91 @@ Write-Host ('Unzip complete')
 '
 <---------------------------- FILER FLYTTES ----------------------------->
 '
+function parentDirMatchNewMainDir {
+    param (
+        $ParentDirName
+    )
+    if ($ParentDirName -eq $newMainFolder) {
+        return $true
+    }else {
+        return $false
+    }
+}
+function charIsSpace {
+    param (
+        $char
+    )
+    if ($char -eq " ") {
+        return $true
+    }else {
+        return $false
+    }
+}
+function stringIsOnlyNumbers {
+    param (
+        $String
+    )
+    if ($String -match '^\d+$') {
+        return $true
+    }else {
+        return $false
+    } 
+}
+function match2DigitFdvDir{
+    param (
+        $ParentDir
+    )
+    $parentDir2Digits = $ParentDir.substring(0, 2)
+
+    $thirdCharIsSpace = charIsSpace $ParentDir.substring(2, 1)
+    $first2DigitsAreNumbers = stringIsOnlyNumbers $parentDir2Digits
+    $matchFdvFolder2Digits = ($parentDir2Digits -eq 17) -Or (($parentDir2Digits -gt 19) -And ($parentDir2Digits -lt 80))
+
+    if ($first2DigitsAreNumbers -And $thirdCharIsSpace -And $matchFdvFolder2Digits) {
+        return $true
+    }else {
+        return $false
+    }
+}
+function match3DigitFdvDir{
+    param (
+        $ParentDir
+    )
+    $parentDir3Digits = $ParentDir.substring(0, 3)
+
+    $fourthCharIsSpace = charIsSpace $ParentDir.substring(3, 1)
+    $first3DigitsAreNumbers = stringIsOnlyNumbers $parentDir3Digits
+    $matchFdvFolder3Digits = ($parentDir3Digits -gt 210) -And ($parentDir3Digits -lt 790)
+
+    if ($first3DigitsAreNumbers -And $fourthCharIsSpace -And $matchFdvFolder3Digits) {
+        return $true
+    }else {
+        return $false
+    } 
+}
 function ShallFileStayInFolder {
     param (
         $File
     )
-    $parentDir = $File.Directory.Name.substring(0, 2)
-    $parentDirName = $File.Directory.Name
+    $parentDir = $File.Directory.Name
 
-    $thirdCharIsSpace = $File.Directory.Name.substring(2, 1) -eq " " 
-    $parentDirMatchNewMainDir = $parentDirName -eq $newMainFolder
-    $parentDirIsNumber = $parentDir -match '^\d+$'
-    $parentDirMatchFdvDirs = ($parentDir -eq 17) -Or ($parentDir -gt 19) -And ($parentDir -lt 80)
-    
-    if (($parentDirIsNumber -And $parentDirMatchFdvDirs -And $thirdCharIsSpace) -Or $parentDirMatchNewMainDir){
-        #stay
+    if (parentDirMatchNewMainDir $parentDir){
+        #file stay
+        return $true
+    } elseif (match2DigitFdvDir $parentDir) {
+        #file stay
+        return $true
+    } elseif (match3DigitFdvDir $parentDir) {
+        #file stay
         return $true
     }
     else {
-        #move
+        #file move
         return $false
     }
 }
 
-#tell flytting tot
+#count total files to move
 $countMovedTot = 0
 $countNotMovedTot = 0
 foreach ($file in (Get-ChildItem -Recurse -File)) {
@@ -83,7 +156,7 @@ foreach ($file in (Get-ChildItem -Recurse -File)) {
     }
 }
 
-#flytt filer
+#moving files:
 while ($true) {
     $countMoved = 0
     $countNotMoved = 0
@@ -138,27 +211,27 @@ Write-Host ('Renaming files - completed')
 '
 <-------------------------- TOMME MAPPER SLETTES ------------------------>
 '
-$totalReturn = 0
+$totalRemoved = 0
 function removeEmptyFolders {
     param (
-        $totalReturn
+        $totalRemoved
     )
-    $foldersRemoved = 0
+    $localRemoved = 0
     Get-ChildItem $path -Recurse -Directory | ForEach-Object {
         if(!(Get-ChildItem -Path $_.FullName)) {
-            $foldersRemoved++
+            $localRemoved++
             Write-Host ("Folder removed: " + $_.Name)
             Remove-Item -Force -Recurse -LiteralPath $_.FullName
         }
     }
-    $totalReturn += $foldersRemoved
-    if ($foldersRemoved -eq 0) {
+    $totalRemoved += $localRemoved
+    if ($localRemoved -eq 0) {
         Write-Host ("Folder removal - complete")
-        return $totalReturn
+        return $totalRemoved
     } else {
-        Write-Host ("Removed: " + $foldersRemoved)
-        Write-Host ("Total removed: " + $totalReturn)
-        removeEmptyFolders $totalReturn  
+        Write-Host ("Removed: " + $localRemoved)
+        Write-Host ("Total removed: " + $totalRemoved)
+        removeEmptyFolders $totalRemoved
     }
 }
 
